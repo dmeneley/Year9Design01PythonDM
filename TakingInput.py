e3a582a93c1a0075b14102994bcbc3a6a010b8ad
Declan Meneley <declan.meneley@y22-design-Declan-Meneley.local>
2018-09-21 15:18:19 -0400

    SlopeCalculator Update

---
+++ TakingInput.py
@@ -0,0 +1,16 @@
+import os
+
+#We use the input function to take an input
+#We have to have an assignment statement to store and input
+
+#input is a function. It runs a small program that
+#causes the computer to stop and wait for input
+fName = input("What is your first name:")
+lName = input("What is your last name:")
+
+print("Hi, "+fName+" "+lName)
+
+initialName = fName[0] + "." + lName #adding strings is concatination
+print("Hi, "+initialName)
+
+os.system("say "+fName+" "+lName)
